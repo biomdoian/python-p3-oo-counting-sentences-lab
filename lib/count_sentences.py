781708e3108595a5d56174d6188b5f24652f9d69
Ian <iankruze60@gmail.com>
2025-05-15 19:15:21 +0300

    Adds modified lib

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
+import re
+import io
+import sys
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        end_punctuations = re.compile(r'[.?!]')
+        sentences = end_punctuations.split(self.value)
+        return sum(1 for s in sentences if s.strip())
+pass
